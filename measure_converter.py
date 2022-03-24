fc8b95ff3707ece502dc74c97ea7a43e664f3d5d
mustafa <mustafagomleksiz4@gmail.com>
2022-03-24 14:07:13 +0000

    The measure converter was created

---
+++ measure_converter.py
@@ -0,0 +1,17 @@
+Task-1:
+
+celsius = float(input("please enter celsius: "))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print('%.2f degree Celsius is equal to %.2f degree Fahrenheit' %(celsius,fahrenheit))
+
+Task-2:
+
+kilometers = float(input("Enter value in kilometers: "))
+
+conversion = 0.621371
+
+miles = kilometers * conversion
+
+print('%.1f kilometers is equal to %.2f miles' %(kilometers,miles))
