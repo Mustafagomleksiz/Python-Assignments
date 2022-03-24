e9a9b420c9ad92e9fdb752569f61519473cc82a2
mustafa <mustafagomleksiz4@gmail.com>
2022-03-24 14:13:36 +0000

    The password reminder was created

---
+++ password_reminder.py
@@ -0,0 +1,15 @@
+username = "Alex"  # you can use any name 
+
+
+
+name = input("Please enter your name : ")
+
+
+
+if name == username  :
+
+    print("Hello, {}! The password is : p24567".format(name))
+
+else :
+
+    print("Hello, {}! See you later.".format(name))
